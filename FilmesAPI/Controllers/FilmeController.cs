110020b36d718f29b20ab7dce414fd39866b9691
robsonmagno12 <magnomon@hotmail.com>
2021-09-14 22:31:31 -0300

    first commit

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,30 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FilmesAPI.Controllers
+{
+    //definindo a classe como um controlador
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+
+        [HttpPost]
+        //precia indicar que  do postman ta vindo requisição com [FromBody]
+        public void AdicionaFilme([FromBody] Filme filme )
+        {
+            filme.Add(filme);
+            //só para validar visualmente para saber o filme que está passando.
+            Console.WriteLine(filme.Titulo);
+
+
+        
+        }
+
+    }
+}
